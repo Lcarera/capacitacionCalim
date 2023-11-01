11a74c2553bb8dee6e0d948ad4d062975779d888
fede <fefechito@gmail.com>
2023-11-01 12:23:35 -0300

    SQL agregado

--- grails-app/services/capacitacioncalim/libro/LibroService.groovy
+++ grails-app/services/capacitacioncalim/libro/LibroService.groovy
@@ -3,14 +3,40 @@ package capacitacioncalim.libro
 import capacitacioncalim.editorial.Editorial
 import capacitacioncalim.editorial.EditorialService
 import grails.transaction.Transactional
+import org.hibernate.transform.Transformers
+import java.util.LinkedHashMap
+
 
 @Transactional
 class LibroService {
 
+    def sessionFactory
     def editorialService
     
     public List<Libro> listLibros() {
-        return Libro.list()
+        //return Libro.list()
+
+        def query= """SELECT 
+        lib.id as id,
+        lib.titulo as t,
+        lib.autor as a,
+        lib.ano as an,
+        edi.nombre as nombre
+        FROM libro lib
+        join editorial edi on edi.id = lib.editorial_id;"""
+
+        def libros = sessionFactory.currentSession.createSQLQuery(query).setResultTransformer(Transformers.aliasToBean(LinkedHashMap)).list().collect{
+            def item = [:]
+            item["id"] = it.id
+            item["titulo"] = it.t
+            item["autor"] = it.a
+            item["ano"] = it.an
+            item["editorial"] = it.nombre
+            
+            return item
+            }    
+        
+        return libros
     } 
 
     public Libro save(LibroCommand command) {
