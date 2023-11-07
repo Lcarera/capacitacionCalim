1d14e59ea48a55fba3cf6ec5630e7c40b39ec522
LSantanderGit <lucasmsantander136@gmail.com>
2023-11-07 12:39:46 -0300

    Fix: App didn't start

---
+++ grails-app/domain/capacitacioncalim/libro/Libro.groovy
@@ -0,0 +1,18 @@
+package capacitacioncalim.libro
+
+import capacitacioncalim.editorial.Editorial
+
+class Libro {
+    String titulo
+    String autor
+    Integer ano
+
+    static belongsTo = [editorial: Editorial]
+    static hasMany = [libroPrecios: LibroPrecio]
+    static constraints = {
+        titulo nullable: false
+        autor nullable: false
+        ano nullable: false
+        editorial nullable: true
+    }
+}
---
+++ grails-app/services/capacitacioncalim/editorial/EditorialService.groovy
@@ -0,0 +1,49 @@
+package capacitacioncalim.editorial
+import grails.transaction.Transactional
+
+@Transactional
+class EditorialService {
+    
+    public List<Editorial> listEditoriales() {
+        Editorial.list()
+    }
+    
+    public Editorial save(EditorialCommand command) {
+        Editorial editorial = new Editorial()
+        editorial.nombre = command.nombre
+        editorial.direccion = command.direccion
+        editorial.anoCreacion = command.anoCreacion
+        editorial.save(flush:true)
+        return editorial
+    }
+
+    public Editorial getEditorial(Long id) {
+        return Editorial.get(id)
+    }
+
+    public Editorial update(EditorialCommand command) {
+        Editorial editorial = Editorial.get(command.id)
+        editorial.nombre = command.nombre
+        editorial.direccion = command.direccion
+        editorial.anoCreacion = command.anoCreacion
+        editorial.save(flush:true)
+        return editorial
+    }
+
+    public Editorial delete(Long id) {
+        Editorial editorial = Editorial.get(id)
+        editorial.delete(flush:true)
+        return editorial
+    }
+
+    def getEditorialCommand(Long id) {
+        def editorial = Editorial.get(id)
+        def editorialCommand = new EditorialCommand()
+        editorialCommand.id = editorial.id
+        editorialCommand.version = editorial.version
+        editorialCommand.nombre = editorial.nombre 
+        editorialCommand.direccion = editorial.direccion
+        editorialCommand.anoCreacion = editorial.anoCreacion
+        return editorialCommand
+    }
+}
