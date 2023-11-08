da113e013980ca64b98cd9127242b9c730db8760
fede <fefechito@gmail.com>
2023-11-08 10:54:04 -0300

    a

--- grails-app/domain/capacitacioncalim/personaje/Personaje.groovy
+++ grails-app/domain/capacitacioncalim/personaje/Personaje.groovy
@@ -12,9 +12,6 @@ class Personaje {
     String gritoGuerra
     static belongsTo = [arma: Arma]
 
-//Cambiar el BelongsTo a Simplemente Arma puede funcionar?
-//pero aun asi el personaje esta fuertemente relacionado al arma, al punto a que es esencial?
-//Llegue a la conclusión con una frase Cliche "El arma es la unica cosa que nunca traicionara a su usuario". EL arma es algo fuertemente relacionado a un personaje de aventuras.
 
     static constraints = {
         nombre nullable: false
--- grails-app/init/capacitacioncalim/BootStrap.groovy
+++ grails-app/init/capacitacioncalim/BootStrap.groovy
@@ -24,3 +24,5 @@ class BootStrap {
     def destroy = {
     }
 }
+
+
