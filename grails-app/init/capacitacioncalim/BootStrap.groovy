d71cad2d0b47235677f184d6c7fa76b147bc677a
valenshinga <valentinshingaki@gmail.com>
2023-11-29 13:59:47 -0300

    afdsasdfasdfasdfasdf

--- grails-app/init/capacitacioncalim/BootStrap.groovy
+++ grails-app/init/capacitacioncalim/BootStrap.groovy
@@ -5,24 +5,5 @@ import capacitacioncalim.inicializacion.JsonInicializacion
 
 class BootStrap {
 
-
-    def init = { servletContext ->
-
-        def registrosCreados = Arma.findAll()
-
-        if (!registrosCreados) {
-            def arco = new Arma(nombre: 'Arco', puntosDeAtaque: 12)
-            def espada = new Arma(nombre: 'Espada',  puntosDeAtaque: 20)
-            def martillo = new Arma(nombre: 'Martillo',  puntosDeAtaque: 24)
-            arco.save(failOnError: true)
-            espada.save(failOnError: true)
-            martillo.save(failOnError: true)
-        }
-        JsonInicializacion.inicializar()
-        Inicializacion.comienzo()
-    }
-    def destroy = {
-    }
-
     
 }
