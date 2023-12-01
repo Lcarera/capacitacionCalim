215fdaea591487f76c8fdd2f9b0a44ef9fb1dbc1
ivanparu <ivanparu04@gmail.com>
2023-12-01 11:16:08 -0300

    .

--- src/main/groovy/capacitacioncalim/personaje/PersonajeCommand.groovy
+++ src/main/groovy/capacitacioncalim/personaje/PersonajeCommand.groovy
@@ -1,29 +1,37 @@
 package capacitacioncalim.personaje
 
+
 import grails.validation.Validateable
 
+
 class PersonajeCommand implements Validateable{
-    
-    Long id
-    Long version
-    
-    String nombre
-    Integer puntosFuerza
-    Integer puntosSalud
-    String gritoGuerra
+  
+   Long id
+   Long version
+  
+   String nombre
+   Integer puntosFuerza
+   Integer puntosSalud
+   String gritoGuerra
 
-    Long armaId
 
-    static constraints = {
-        id nullable: true
-        version nullable: true
-        
-        nombre nullable: false
-        puntosFuerza nullable: false
-        puntosSalud nullable: false
-        gritoGuerra nullable: true
+   Long armaId
+   Long userId
 
-        armaId nullable: true
 
-    }
-} 
+   static constraints = {
+       id nullable: true
+       version nullable: true
+      
+       nombre nullable: false
+       puntosFuerza nullable: false
+       puntosSalud nullable: false
+       gritoGuerra nullable: true
+
+
+       armaId nullable: false
+       userId nullable: false
+
+
+   }
+}
