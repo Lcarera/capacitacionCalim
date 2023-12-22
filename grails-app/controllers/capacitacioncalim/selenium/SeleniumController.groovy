23eec749c1b979607b17b438c3de3062c7c19976
ivanparu <ivanparu04@gmail.com>
2023-12-22 12:41:26 -0300

    Avances

---
+++ grails-app/controllers/capacitacioncalim/selenium/SeleniumController.groovy
@@ -0,0 +1,36 @@
+import grails.plugin.springsecurity.annotation.Secured
+import org.openqa.selenium.WebDriver
+import org.springframework.web.servlet.ModelAndView
+
+
+
+
+class SeleniumController{
+    
+    def seleniumService
+    
+    def index() {
+        render (view: 'indexsel')
+    }
+
+    def buscarUrl(String tituloVideo){
+        WebDriver driver = seleniumService.inicializarDriver("");
+        try{
+            driver.get("https://www.youtube.com/")
+
+            /*WebElement campoBusqueda = driver.findElement(By.name("search_query"))
+            campoBusqueda.sendKeys(tituloVideo)
+            campoBusqueda.submit()
+
+            WebElement primerResultado = driver.findElement(By.id("video-title"))
+            primerResultado.click()
+
+            Thread.sleep(5000)*/
+        } catch (Exception e) {
+            e.printStackTrace()
+            return new ModelAndView("error").addObject("mensajeError", "Ocurrió un error al buscar el video.")
+    }
+
+}
+
+}
---
+++ grails-app/views/selenium/indexsel.gsp
@@ -0,0 +1,4 @@
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Introducir titulo de video</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
